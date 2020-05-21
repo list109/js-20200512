365537395e176c4cc4dbfb26e8250942fb01ada2
Vladimit Bonadernko <list109@list.ru>
2020-05-21 19:13:07 +0300

    more short notice to check out undefined value

--- 02-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 02-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,8 +4,8 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-    return obj?.hasOwnProperty && Object.entries(obj).reduce((prev, [key, value]) => {
+    return obj && Object.entries(obj).reduce((prev, [key, value]) => {
         prev[value] = key;
         return prev;
-    }, {})
+    }, {});
 }
