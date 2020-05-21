0f245de630aa1690f8be6714ab5177ed6000fbc0
Vladimit Bonadernko <list109@list.ru>
2020-05-21 13:01:44 +0300

    the solution of the second task

--- 02-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 02-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,8 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-
+    return obj?.hasOwnProperty && Object.entries(obj).reduce((prev, [key, value]) => {
+        prev[value] = key;
+        return prev;
+    }, {})
 }
