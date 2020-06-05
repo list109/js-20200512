36787445b6d5db8bd77e5b9cb211af36e5fac5a4
Vladimir <list109@list.ru>
2020-06-05 11:28:49 +0300

    finished the sortable-table-v2

--- 05-events-practice/1-sortable-table-v2/src/index.js
+++ 05-events-practice/1-sortable-table-v2/src/index.js
@@ -2,11 +2,25 @@ export default class SortableTable {
     element;
     subElements = {};
 
-    constructor(header, { data } = {}) {
+    sortByHead = event => {
+        const head = event.target.closest('[data-sortable]');
+        if (!head || head.dataset.sortable !== "true") return;
+
+        const order = (head.dataset.order === 'asc') ? 'desc' : 'asc';
+        const field = head.dataset.id;
+        this.sort({ id: field, order: order });
+    }
+
+    constructor(header, {
+        data = [],
+        sorted = { id: 'title', order: 'asc' }
+    } = {}) {
         this.header = header;
         this.data = data;
 
         this.render();
+        this.initEventListeners();
+        this.sort(sorted);
     }
 
     render() {
@@ -16,15 +30,21 @@ export default class SortableTable {
 
         this.element = element.firstElementChild;
 
+        const cell = this.element.querySelector('.sortable-table__cell');
+        cell.insertAdjacentHTML('beforeend', this.arrowTemplate);
+
         this.subElements = this.getSubElements(this.element);
     }
 
-    getSubElements(element) {
+    initEventListeners() {
+        this.element.addEventListener('click', this.sortByHead);
+    }
+
+    getSubElements() {
         const elements = this.element.querySelectorAll('[data-element]');
 
         return [...elements].reduce((accum, subElement) => {
             accum[subElement.dataset.element] = subElement;
-           
             return accum;
         }, {})
     }
@@ -39,10 +59,9 @@ export default class SortableTable {
             </div>`
     }
 
-    headerRowTemplate({id, title, sortable}) {
-        return `<div class="sortable-table__cell" data-id="${id} data-sortable="${sortable}>
+    headerRowTemplate({ id, title, sortable }) {
+        return `<div class="sortable-table__cell" data-id="${id}" data-sortable="${sortable}" data-order="asc">
             <span>${title}</span>
-            ${this.arrowTemplate}
         </div>`
     }
 
@@ -66,33 +85,46 @@ export default class SortableTable {
             ${this.header
                 .map(head => head.template?.(obj[head.id]) || `<div class="sortable-table__cell">${obj[head.id]}</div>`)
                 .join('')}
-            </div>`
+            </div>`;
     }
 
-    sort(field, order) {
+    sort({ id: field='title', order='asc'}) {
         const fieldIndex = this.header.findIndex(item => item.id === field);
         const body = this.subElements.body;
+        const { sortType, customSorting } = this.header[fieldIndex];
+        const currentHead = this.subElements.header.children[fieldIndex];
+
+        currentHead.append(this.subElements.arrow);
+        currentHead.dataset.order = order;
 
         const options = {
-            asc: 1,
-            desc: -1,
+            asc: -1,
+            desc: 1,
         }
 
-        order = options[order] || 1;
+        const direction = options[order] || 1;
 
-        function sortMethod(arr, order) {
+        body.append(...sortMethod([...body.children], direction));
+
+        function sortMethod(arr, direction) {
             return arr.sort((a, b) => {
                 a = a.children[fieldIndex].textContent;
                 b = b.children[fieldIndex].textContent;
-                return order * a.localeCompare(b, 'ru', { caseFirst: 'upper', numeric: true });
+                switch (sortType) {
+                    case 'custom':
+                        return direction * customSorting(a, b);
+                    case 'string':
+                    case 'number':
+                    default:
+                        return direction * a.localeCompare(b, 'ru', { caseFirst: 'upper', numeric: true });
+                }
             })
         }
-
-        body.append(...sortMethod([...body.children], order));
     }
 
     destroy() {
         this.remove();
+        this.element.removeEventListener('click', this.sortByHead);
     }
 
     remove() {
